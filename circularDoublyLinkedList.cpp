249354a32c6d1882460ade9cb23d32692bfb04e1
Muhammad Hammad Asghar <101011203+Muhammad-Hammad-Asghar-56@users.noreply.github.com>
2022-11-09 11:49:54 +0500

    Fix the Issue of the InsertHead,End and Sorting and Reverse

--- circularDoublyLinkedList.cpp
+++ circularDoublyLinkedList.cpp
@@ -25,145 +25,192 @@ class LinkList
 {
 public:
     Node *head = new Node();
+    Node *Tail = new Node();
 
 public:
-    LinkList(void) { head = NULL; }                     // constructor
-    LinkList(Node *headPointer) { head = headPointer; } // constructor
+    LinkList(void)
+    {
+        head = NULL;
+        Tail = NULL;
+    }; // constructor
+    LinkList(Node *headPointer)
+    {
+        head = headPointer;
+        Tail = headPointer;
+    }; // constructor
 
     ~LinkList(void); // destructor
     bool isEmpty() { return head == NULL; }
-    Node *insertNode(int index, int x) // insert at the given index
-    {
-        if (index == 0)
-        {
-            insertAtHead(x);
-        }
-        else
-        {
-            Node *current = head;
-            int i = 0;
-            while (current != NULL && i < index)
-            {
-                current = current->next;
-                i++;
-            }
-            Node *newNode = new Node(x);
-            newNode->previous = current->previous;
-            newNode->next = current;
-            current->previous = newNode;
-        }
-    }
     Node *insertAtHead(int x) // insert at start of list
     {
         Node *newNode = new Node(x);
-        newNode->next = head;
         if (head != NULL)
         {
             head->previous = newNode;
+            newNode->next = head;
+            head = newNode;
+            head->previous = Tail;
         }
-        head = newNode;
+        else
+        {
+            head = newNode;
+            head->previous = Tail;
+            head->next = head;
+        }
+
+        if (Tail == NULL) // Tail is empty
+        {
+            Tail = head;
+            head->previous = Tail;
+        }
+        else
+        {
+            head->previous = Tail;
+            Tail->next = head;
+        }
     }
     Node *insertAtEnd(int x) // insert at end of list
     {
-        Node *currentNodePointer = head;
-        while (currentNodePointer->next != NULL)
-        {
-            currentNodePointer = currentNodePointer->next;
-        }
         Node *newNode = new Node(x);
-        newNode->previous = currentNodePointer;
-        currentNodePointer->next = newNode;
+        if (head == NULL)
+        {
+            return insertAtHead(x);
+        }
+        Tail->next = newNode;
+        newNode->previous = Tail;
+        newNode->next = head;
+        head->previous = newNode;
+        Tail = newNode;
+        return newNode;
+    }
+    Node *insertNode(int index, int x) // insert at the given index
+    {
+
+        Node *current = head;
+        int i = 0;
+        while (i < index)
+        {
+            current = current->next;
+            i++;
+        }
+        if (current == head)
+        {
+            insertAtHead(x);
+        }
+        else if (current == Tail)
+        {
+            insertAtEnd(x);
+        }
+        else
+        {
+
+            Node *newNode = new Node(x);
+
+            newNode->previous = current->previous;
+            newNode->next = current;
+
+            current->previous->next = newNode;
+            current->previous = newNode;
+        }
     }
     bool findNode(int x) // search for data value x in the list
     {
         Node *currentNodePointer = head;
-        while (currentNodePointer->next != NULL)
+        if (currentNodePointer != NULL)
         {
-            if (currentNodePointer->value == x)
+            do
             {
-                return true;
-            }
-            else
-            {
-                currentNodePointer = currentNodePointer->next;
-            }
+                if (currentNodePointer->value == x)
+                {
+                    return true;
+                }
+                else
+                {
+                    currentNodePointer = currentNodePointer->next;
+                }
+            } while (currentNodePointer != head);
         }
         return false;
     }
     bool deleteNode(int x) // delete all occurrences of x
     {
         Node *currentNode = head;
-        while (currentNode->next != NULL)
+        if (currentNode != NULL)
         {
-            if (currentNode->value == x)
+            do
             {
-                Node *previous = currentNode->previous;
-                Node *next = currentNode->next;
-                if (previous == NULL)
-                { // its mean the node is the first in the list
-                    head = next;
-                }
-                else
+                if (currentNode->value == x)
                 {
-                    previous->next = next;
-                    next->previous = previous;
+                    Node *next = currentNode->next;
+                    if (currentNode == head)
+                    { // its mean the node is the first in the list
+                        deleteFromStart();
+                    }
+                    else if (currentNode == Tail)
+                    { // its mean the node is the first in the list
+                        deleteFromEnd();
+                    }
+                    else
+                    {
+                        currentNode->previous->next = currentNode->next;
+                        currentNode->next->previous = currentNode->previous;
+                    }
                 }
-            }
 
-            currentNode = currentNode->next;
+                currentNode = currentNode->next;
+            } while (currentNode != head);
         }
+        return 1;
     }
     bool deleteFromStart() // deletes starting node of list
     {
         head = head->next;
-        head->previous = NULL;
+        head->previous = Tail;
+        Tail->next = head;
     }
     bool deleteFromEnd() // deletes last node of list
     {
-        Node *currentNode = head;
-        while (true)
-        {
-            if (currentNode->next == NULL)
-            {
-                currentNode->previous->next = NULL;
-                break;
-            }
-            else
-            {
-                currentNode = currentNode->next;
-            }
-        }
+        Tail = Tail->previous;
+        Tail->next = head;
+        head->previous = Tail;
     }
     void displayList(void)
     {
         Node *currentNode = head;
-        while (currentNode != NULL)
+        do
         {
             cout << currentNode->value << endl;
             currentNode = currentNode->next;
-        }
+
+        } while (currentNode != head);
     }
     Node *reverseList()
     { // reverses the linklist and returns a new list
+        Node *start = head;
+        Node *end = Tail;
         Node *current = head;
-        while (current != NULL)
+        do
         {
-            Node *temp = current->next;
-            current->next = current->previous;
-            current->previous = temp;
-            head = current;
-            current = temp;
-        }
+            Node *nexttemp = current->next;
+            Node *prevtemp = current->previous;
+            current->next = prevtemp;
+            current->previous = nexttemp;
+            current = nexttemp;
+        } while (current != start);
+        // current->next=end;
+        // current->previous=current->next;
+        Tail = start;
+        head = end;
         return head;
     }
     Node *sortListBubbleSort(Node *list)
     { // sorts the input-ed list
         int temp = 0;
         Node *unSortedElement = list;
-        while (unSortedElement != NULL)
+        do
         {
             Node *checkElementsPointer = unSortedElement->next;
-            while (checkElementsPointer != NULL)
+            while (checkElementsPointer != head)
             {
                 if (checkElementsPointer->value < unSortedElement->value)
                 {
@@ -174,27 +221,23 @@ public:
                 checkElementsPointer = checkElementsPointer->next;
             }
             unSortedElement = unSortedElement->next;
-        }
+        } while (unSortedElement != head);
     }
     Node *sortByInsertionSort(Node *list)
     {
         Node *currentPointer = list;
-        while (currentPointer != NULL)
+        do
         {
             int value = currentPointer->value;
             Node *previous = currentPointer->previous;
-            while (previous->next != NULL && value <= previous->value)
+            while (previous->next != head && value <= previous->value)
             {
                 previous->next->value = previous->value;
                 previous = previous->previous;
             }
-            if (previous->value <= value)
-            {
-                previous->next->value = previous->value;
-            }
-            previous->value=value;
+            previous->next->value = value;
             currentPointer = currentPointer->next;
-        }
+        } while (currentPointer != head);
         return NULL;
     }
     Node *removeDuplicates(Node *list)
@@ -231,8 +274,9 @@ public:
     { // results contains intersection of two lists
         LinkList *interestedList = new LinkList();
         Node *currentCheckPointerList1 = list1;
-        while (currentCheckPointerList1 != NULL)
+        do
         {
+            // if (isValueExisted(currentCheckPointerList1, list2))
             if (isValueExisted(currentCheckPointerList1, list2))
             {
                 if (interestedList->head == NULL)
@@ -245,7 +289,7 @@ public:
                 }
             }
             currentCheckPointerList1 = currentCheckPointerList1->next;
-        }
+        } while (currentCheckPointerList1 != head);
         return interestedList->head;
     }
 
@@ -253,10 +297,15 @@ public:
     bool isValueExisted(Node *key, Node *list)
     {
         Node *currentNode = list;
-        while (currentNode != NULL && currentNode != key)
+        while (currentNode != list->previous)
         {
+            if (currentNode != key)
+            {
+                break;
+            }
             if (currentNode->value == key->value)
             {
+                currentNode->previous->next = currentNode->next;
                 return true;
             }
             else
@@ -269,7 +318,7 @@ public:
     bool isValueExisted(Node *key)
     {
         Node *currentNode = key->previous;
-        while (currentNode->previous != NULL && currentNode != key)
+        while (currentNode->previous != Tail && currentNode != key)
         {
             if (currentNode->value == key->value)
             {
@@ -286,20 +335,20 @@ public:
 main()
 {
     /*                                     Intersection */
-    // LinkList *list = new LinkList();
-    // list->insertAtHead(10);
-    // list->insertAtEnd(20);
-    // list->insertAtEnd(30);
+    LinkList *list = new LinkList();
+    list->insertAtHead(10);
+    list->insertAtEnd(20);
+    list->insertAtEnd(30);
 
-    // LinkList *list2 = new LinkList();
-    // list2->insertAtHead(40);
-    // list2->insertAtEnd(20);
-    // list2->insertAtEnd(30);
+    LinkList *list2 = new LinkList();
+    list2->insertAtHead(40);
+    list2->insertAtEnd(20);
+    list2->insertAtEnd(30);
 
-    // Node *newlistHead = list->interestLists(list->head, list2->head);
-    // LinkList *IntersectList = new LinkList(newlistHead);
-    // cout << "-------------------" << endl;
-    // IntersectList->displayList();
+    Node *newlistHead = list->interestLists(list->head, list2->head);
+    LinkList *IntersectList = new LinkList(newlistHead);
+    cout << "-------------------" << endl;
+    IntersectList->displayList();
     // _________________________________________________________________________________________________
     /*                                      Merge */
     // LinkList *list = new LinkList();
@@ -319,12 +368,40 @@ main()
     /*                                      Insert At */
     // LinkList *list = new LinkList();
     // list->insertAtHead(10);
+    // list->insertAtEnd(30);
+    // list->insertAtEnd(50);
+    // list->insertAtEnd(100);
+    // // list->insertNode(0, 20);
+    // // list->insertNode(1, 50);
+    // // list->deleteNode(10);
+    // // list->deleteNode(100);
+    // // list->deleteFromStart();
+    // // list->deleteFromEnd();
+    // list->displayList();
+    //__________________________________________________________________________________________________
+    /*                                      Delete */
+    // LinkList *list = new LinkList();
+    // list->insertAtHead(10);
+    // list->insertAtEnd(30);
+    // list->insertAtEnd(50);
+    // list->insertAtEnd(100);
+    // list->deleteNode(10);
+    // list->deleteNode(100);
+    // list->deleteFromStart();
+    // list->deleteFromEnd();
+    // list->displayList();
+    //__________________________________________________________________________________________________
+    /*                                      Reverse */
+    // LinkList *list = new LinkList();
+    // list->insertAtHead(10);
     // list->insertAtEnd(20);
     // list->insertAtEnd(30);
-    // list->insertNode(0, 50);
-    // // list->deleteNode(20);
-    // // list->deleteFromStart();
-    // list->deleteFromEnd();
+    // list->insertNode(20,90);
+    // list->insertNode(5,40);
+    // list->insertAtEnd(50);
+    // list->insertAtEnd(100);
+    // list->displayList();
+    // list->reverseList();
     // list->displayList();
 
     // _________________________________________________________________________________________________
@@ -337,6 +414,7 @@ main()
     // list->insertAtEnd(210);
     // list->insertAtEnd(220);
     // list->insertAtEnd(0);
-    // list->sortList(list->head);
+    // list->sortListBubbleSort(list->head);
+    // // list->sortByInsertionSort(list->head);
     // list->displayList();
 }
